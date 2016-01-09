dcc4ddb029eb3830afc4ed3bc271c0860e0d5ea9
Kate <katerebekah@gmail.com>
2016-01-09 10:26:28 -0600

    updating the front end

--- worldDataWeb/Scripts/app.js
+++ worldDataWeb/Scripts/app.js
@@ -1,13 +1,20 @@
 ﻿var app = angular.module('worldData', []);
 var webaddress = 'http://localhost:61643'
 app.controller('profileCtrl', function ($scope, $http) {
+    $scope.baseApiAddress = "https://www.quandl.com/data/";
+    $scope.apiKey = "&api_key=mWbezmtRyiryCF8GP5sN";
     $scope.chart = {
         chartId: chartId,
-        cities: []
+        cities: [],
+        data: []
     }
+    $scope.CreateChart = function () { }
+    $scope.GoBack = function () { $scope.Step = $scope.Step - 1;}
+    $scope.Step = 1;
     $scope.Countries;
     $scope.Cities;
     $scope.GetCountries = function () {
+        $scope.Step = 2;
         console.log("firing");
         $http.get('http://localhost:61643/api/City/GetCountries').success(function (response) {
             $scope.Countries = response;
@@ -19,6 +26,7 @@ app.controller('profileCtrl', function ($scope, $http) {
         });
     }
     $scope.GetCitiesinCountry = function (id) {
+        $scope.Step = 3;
         $http.get('http://localhost:61643/api/City/GetCitiesInCountry/?cityId=' + id).success(function (response) {
             $scope.Cities = response;
             $scope.Cities.sort(function compareNumbers(a, b) {
@@ -31,5 +39,9 @@ app.controller('profileCtrl', function ($scope, $http) {
     $scope.AddCityToChart = function (city) {
         console.log(city);
         $scope.chart.cities.push(city);
+        $scope.Step = 1;
+        $http.get($scope.baseApiAddress + city.ApiURL + $scope.apiKey)
+            .then(function (response) { console.log(response) })
+            .then(function (error) { console.log(error) });
     }
 });
